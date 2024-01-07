96e116c8c6d3c4db72c3e5c1bf6372e119e8a293
Maylebuh <malebomawela96@gmail.com>
2024-01-07 10:29:42 -0800

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -5,21 +7,21 @@
  * @h: pointer to head of list
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *current;
-	unsigned int n; /* number of nodes */
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
 
-	current = h;
-	n = 0;
-	while (current != NULL)
-	{
-	       	printf("%i\n", current->n);
-		current = current->next;
-		n++;
-	}
-	return (n);
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
+    }
+
+    return (n);
 }
 
 /**
@@ -28,29 +30,30 @@ size_t print_listint(const listint_t *h)
  * @n: integer to be included in new node
  * Return: address of the new element or NULL if it fails
  */
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new;
-	listint_t *current;
-	current = *head;
+    listint_t *new;
+    listint_t *current;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
+    current = *head;
 
-  if (*head == NULL)
-    *head = new;
-  else
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->next = NULL;
+
+    if (*head == NULL)
+        *head = new;
+    else
     {
-      while (current->next != NULL)
-	current = current->next;
-      current->next = new;
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new;
     }
 
-  return (new);
+    return (new);
 }
 
 /**
@@ -60,12 +63,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
  */
 void free_listint(listint_t *head)
 {
-  listint_t *current;
+    listint_t *current;
 
-  while (head != NULL)
+    while (head != NULL)
     {
-      current = head;
-      head = head->next;
-      free(current);
+        current = head;
+        head = head->next;
+        free(current);
     }
-}
