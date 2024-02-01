caa2bf2188c2f6041f08462747279a63bdf2c260
Maylebuh <malebomawela96@gmail.com>
2024-02-01 10:25:55 -0800

    0x07. Python - Test-driven development

---
+++ 0x07-python-test_driven_development/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+def add_integer(a, b=98):
+def matrix_divided(matrix, div):
+def say_my_name(first_name, last_name=""):
+def print_square(size):
+def text_indentation(text):
+def max_integer(list=[]):
+def matrix_mul(m_a, m_b):
+def lazy_matrix_mul(m_a, m_b):
+void print_python_string(PyObject *p);
+
+#endif
