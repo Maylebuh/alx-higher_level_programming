98b0e95b7f5879ecd7f344c9392cdc8d949d6719
Maylebuh <malebomawela96@gmail.com>
2024-04-25 11:24:56 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request, and displays the body of the response for a 200 status code
 
-curl -sL -w "%{http_code}" -o /dev/null "$1" | {
-    read -r status
-    if [ "$status" = "200" ]; then
-        curl -sL "$1"
-    fi
-}
+response=$(curl -sL -w "%{http_code}" -o /dev/null "$1")
+status_code=$(echo "$response" | tail -n1)
+
+if [ "$status_code" -eq 200 ]; then
+    curl -sL "$1"
+fi
